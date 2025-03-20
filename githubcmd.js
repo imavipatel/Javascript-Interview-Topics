514ba938d371893032ae2d7beb047c4be45d7339
Ankit Patel <ankit.patel4@aubank.in>
2025-03-20 18:29:58 +0530

    Added git hub cmd

---
+++ githubcmd.js
@@ -0,0 +1,9 @@
+/*
+echo "# Javascript-Interview-Topics" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/imavipatel/Javascript-Interview-Topics.git
+git push -u origin main
+*/
