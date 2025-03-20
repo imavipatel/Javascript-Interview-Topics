33a8a3cc14fcc52b25c2ae53284fb0e0c8368444
Ankit Patel <ankit.patel4@aubank.in>
2025-03-20 18:27:41 +0530

    first commit

---
+++ Scope.js
@@ -0,0 +1,7 @@
+//Scope
+
+/* 
+Scope determines the accessibility of varaible object and functions.
+In Javascript, a variable has 3 types of scope
+1. block scope 2. function scope 3. global scope
+*/
