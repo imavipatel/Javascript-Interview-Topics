4bbdae273b88f57c9febc545c748d8334492f1a4
Ankit Patel <ankit.patel4@aubank.in>
2025-03-20 18:32:47 +0530

    Added closure

---
+++ closure.js
@@ -0,0 +1,29 @@
+//Closure
+/* Closure means in Javascript that an inner function always has 
+access to the variable of it outer function even after the outer function has returned.
+1. It has access to its own scope( variable defined between its curly brackets);
+2. It has access to its outer functions variables
+3, It has access to the global variables.
+
+Example====>
+*/
+
+let globalVariable = 'I am global variable'
+
+function outerFunction(){
+    let outerVariable = 'I am outer function variable'
+    function innerFunction(){
+        let innerVariable ='I am inner function variable'
+
+        console.log(outerVariable);
+        console.log(innerVariable)
+        console.log(globalVariable)
+        console.log(lowerGlobalVariable)
+    }
+    return innerFunction()
+}
+
+let lowerGlobalVariable = 'I am lower global variable'
+
+const outputFunction = outerFunction()
+outerFunction()
